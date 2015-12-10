52308828cb4a7504daf191de91b7527fa9d90e7e
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-10 13:23:03 -0600

    coma

--- finalresults.py
+++ finalresults.py
@@ -22,7 +22,7 @@ if total > 1:
 
     input_word = wrapper(sys.argv[1])
     print input_word
-    cur.execute("SELECT count FROM Tweetwordcount WHERE word=%s AND word=%s", (input_word,input_word))
+    cur.execute("SELECT count FROM Tweetwordcount WHERE word=%s", (input_word,))
     input_count = cur.fetchall()
     print " Total number of occurences of %s: %s"%(input_word,input_count)
 else:
