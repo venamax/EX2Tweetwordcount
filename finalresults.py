e193b0e4ec267f933ad327a79b6115dd8fda1ae7
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-10 08:49:16 -0600

    fix

--- finalresults.py
+++ finalresults.py
@@ -11,6 +11,7 @@ cmdargs = str(sys.argv)
 # Print it
 print ("The total numbers of args passed to the script: %d " % total)
 print ("Args list: %s " % cmdargs)
+print cmdargs[1]
 
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
@@ -18,7 +19,7 @@ cur = conn.cursor()
 if total > 1:
     input_word = cmdargs[1]
     print input_word
-    cur.execute("SELECT count FROM Tweetwordcount WHERE word='%s'", (input_word))
+    cur.execute("SELECT count FROM Tweetwordcount WHERE word=%s", (input_word))
     input_count = cur.fetchall()
     print "Total number of occurences of ""%s"": %s"%(input_word,input_count[0])
 else:
