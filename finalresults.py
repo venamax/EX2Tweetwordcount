6100adc3a2fd2241c57ab282f2b52c8e9e88372d
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-10 08:52:38 -0600

    test

--- finalresults.py
+++ finalresults.py
@@ -7,17 +7,18 @@ total = len(sys.argv)
  
 # Get the arguments list 
 cmdargs = str(sys.argv)
+
  
 # Print it
 print ("The total numbers of args passed to the script: %d " % total)
 print ("Args list: %s " % cmdargs)
-print cmdargs[1]
+
 
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
     
 if total > 1:
-    input_word = cmdargs[1]
+    input_word = cmdargs[0]
     print input_word
     cur.execute("SELECT count FROM Tweetwordcount WHERE word=%s", (input_word))
     input_count = cur.fetchall()
