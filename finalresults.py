8d4d52d87c544050b248fbf43c3648c68d76ee2f
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-10 12:41:31 -0600

    test

--- finalresults.py
+++ finalresults.py
@@ -1,6 +1,6 @@
 import psycopg2
 import sys
-import codecs
+
  
 # Get the total number of args passed to the finalresults.py
 total = len(sys.argv)
@@ -8,6 +8,8 @@ total = len(sys.argv)
 # Get the arguments list 
 cmdargs = str(sys.argv)
 
+def wrapper(s1):
+    return "'%s'" % s1
  
 # Print it
 print ("The total numbers of args passed to the script: %d " % total)
@@ -17,10 +19,11 @@ conn = psycopg2.connect(database="tcount", user="postgres", password="pass", hos
 cur = conn.cursor()
     
 if total > 1:
-    print sys.argv[1]
-    cur.execute(" SELECT count FROM Tweetwordcount WHERE word=%s ", str(sys.argv[1]))
+    input_word = wrapper(sys.argv[1])
+    print input_word
+    cur.execute(" SELECT count FROM Tweetwordcount WHERE word=%s ", input_word))
     input_count = cur.fetchall()
-    print """ Total number of occurences of "%s": %s"""%(sys.argv[1],input_count[0][0])
+    print """ Total number of occurences of "%s": %s"""%(input_word,input_count[0][0])
 else:
     print "yeahhnnnh"
 
