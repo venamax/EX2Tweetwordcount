ad7969299f5556b52f6f29fd30549e7bbd8f73aa
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-09 23:39:07 -0600

    word

--- finalresults.py
+++ finalresults.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-input_word = raw_input(str(input))
+input_word = input(str(word_entered))
 
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
