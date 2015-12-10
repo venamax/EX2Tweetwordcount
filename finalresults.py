c9eae4358ba79c4ff2b922fcd111004f12a50a18
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-10 07:24:48 -0600

    new approach

--- finalresults.py
+++ finalresults.py
@@ -1,18 +1,23 @@
 import psycopg2
+import cmd, sys
 
-input_word = raw_input('--> ')
-input_word = str(input_word)
+def emptyline(self):
+        """Called when an empty line is entered in response to the prompt.
 
+        If this method is not overridden, it repeats the last nonempty
+        command entered.
 
-conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
-cur = conn.cursor()
+        """
+        conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+        cur = conn.cursor()
+        
+        if self.lastcmd:
+            self.lastcmd = ""
+            return self.onecmd('\n')
+        else: 
+            input_word = self.lastcmd
+            print word
+            cur.execute("SELECT count FROM Tweetwordcount WHERE word=%s", (input_word))
+            input_count = cur.fetchall()
+            print "Total number of occurences of ""%s"": %s"%(input_word, input_count)
 
-
-if input_word != '':
-
-    cur.execute("SELECT count FROM Tweetwordcount WHERE word=%s", (input_word[1:-1]))
-    input_count = cur.fetchall()
-    print "Total number of occurences of ""%s"": %s"%(input_word, input_count)
-
-else:
-    print "O h yeah"
