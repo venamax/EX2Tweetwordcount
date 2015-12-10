95ecd32736fbd2a5d1e844c207d5b671b711b516
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-10 10:12:56 -0600

    values

--- finalresults.py
+++ finalresults.py
@@ -20,8 +20,8 @@ cur = conn.cursor()
 if total > 1:
     input_word = str(sys.argv[1])
     input_word = dict(input_word=input_word)
-    print input_word
-    cur.execute("SELECT count FROM Tweetwordcount WHERE word=%s",input_word)
+    print input_word.values
+    cur.execute("SELECT count FROM Tweetwordcount WHERE word=%s",input_word.values)
     input_count = cur.fetchall()
     print """ Total number of occurences of "%s": %s"""%(input_word,input_count[0][0])
 else:
