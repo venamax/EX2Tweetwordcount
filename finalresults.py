f6f703e83db685dd80f8bf35322ab7ea6db57da4
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-10 08:12:00 -0600

    another test

--- finalresults.py
+++ finalresults.py
@@ -1,25 +1,32 @@
+
 import psycopg2
-import cmd, sys
+from cmd import Cmd
 
-def emptyline(self):
-        """Called when an empty line is entered in response to the prompt.
+class MyPrompt(Cmd):
+    
+    conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+    cur = conn.cursor()
+    
+    def have_word(self, args):
+        """find the count for a given word"""
+        if len(args) == 0:
+            print "No argument"
+        else:
+            input_word = args
+            
+        print input_word
+        cur.execute("SELECT count FROM Tweetwordcount WHERE word='%s'", (input_word))
+        input_count = cur.fetchall()
+        print "Total number of occurences of ""%s"": %s"%(input_word,input_count[0])
 
-        If this method is not overridden, it repeats the last nonempty
-        command entered.
 
-        """
-        conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
-        cur = conn.cursor()
-        
-        if self.lastcmd:
-            self.lastcmd = ""
-            return self.onecmd('\n')
-        else: 
-            input_word = self.lastcmd
-            print input_word
-            cur.execute("SELECT count FROM Tweetwordcount WHERE word='%s'", (input_word))
-            input_count = cur.fetchall()
-            print "Total number of occurences of ""%s"": %s"%(input_word,input_count[0])
+    def do_quit(self, args):
+        """Quits the program."""
+        print "Quitting."
+        raise SystemExit
 
-emptyline(cmd.Cmd)
 
+if __name__ == '__main__':
+    prompt = MyPrompt()
+    prompt.prompt = '> '
+    prompt.cmdloop('Starting prompt...')
