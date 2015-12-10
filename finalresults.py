095755b1b72ecf276eac5a68b854fa9318c9f4f1
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-10 09:44:28 -0600

    test

--- finalresults.py
+++ finalresults.py
@@ -22,7 +22,7 @@ if total > 1:
     print input_word
     cur.execute("SELECT count FROM Tweetwordcount WHERE word='Capriles'")
     input_count = cur.fetchall()
-    print "Total number of occurences of %s: %s"%(input_word,input_count[0][0])
+    print """ Total number of occurences of "%s": %s"""%(input_word,input_count[0][0])
 else:
     print "yeahhh"
 
