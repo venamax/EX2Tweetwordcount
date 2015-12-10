8d2923e5e124c752f12b8d91b0ea1357960f79f3
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-09 23:50:41 -0600

    test

--- finalresults.py
+++ finalresults.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-input_word = raw_input("Word to find:")
+input_word = raw_input('--> ')
 
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
@@ -8,7 +8,7 @@ cur = conn.cursor()
 
 if input_word != '':
 
-    cur.execute("SELECT count from Tweetwordcount WHERE word=%s", (str(input_word)))
+    cur.execute("SELECT count from Tweetwordcount WHERE word=%s", (input_word))
     input_count = cur.fetchall()
     print "Total number of occurences of ""%s"": %s"%(input_word, input_count)
 
