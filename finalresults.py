f440e6f5d8d7047360b7ccc4b0fe01538b770985
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-10 14:20:36 -0600

    final version

--- finalresults.py
+++ finalresults.py
@@ -11,17 +11,13 @@ cmdargs = str(sys.argv)
 def wrapper(s1):
     return """%s""" % s1
  
-# Print it
-print ("The total numbers of args passed to the script: %d " % total)
-print ("Args list: %s " % cmdargs)
 
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
     
 if total > 1:
 
-    input_word = wrapper(sys.argv[1])
-    print input_word
+    input_word = sys.argv[1]
     cur.execute("""SELECT count FROM Tweetwordcount WHERE word=%s""", (input_word,))
     input_count = cur.fetchall()
     print """ Total number of occurences of "%s": %s"""%(input_word,input_count[0][0])
