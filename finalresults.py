0cc56000274c557fcc4dbb2d8ad08b7a5f4255e9
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-10 07:54:51 -0600

    '

--- finalresults.py
+++ finalresults.py
@@ -17,7 +17,7 @@ def emptyline(self):
         else: 
             input_word = self.lastcmd
             print input_word
-            cur.execute("SELECT count FROM Tweetwordcount WHERE word=Chavez")
+            cur.execute("SELECT count FROM Tweetwordcount WHERE word='Chavez'")
             input_count = cur.fetchall()
             print "Total number of occurences of ""Chavez"": %s"%(input_count)
 
