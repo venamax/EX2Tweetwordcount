95cebe341023d3dd5269e3f33ea87bdc79826adb
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-12 19:21:27 -0600

    if not exist added

--- src/bolts/wordcount.py
+++ src/bolts/wordcount.py
@@ -16,7 +16,7 @@ class WordCounter(Bolt):
         word = tup.values[0]
         conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
         cur = conn.cursor()  
-        cur.execute('''CREATE TABLE Tweetwordcount(word TEXT PRIMARY KEY NOT NULL, count INT NOT NULL);''')
+        cur.execute('''CREATE TABLE IF NOT EXISTS Tweetwordcount(word TEXT PRIMARY KEY NOT NULL, count INT NOT NULL);''')
         conn.commit()
         initial_count = 1
         
