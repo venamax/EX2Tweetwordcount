547177fdb865c1418147fd657d88ae2baf7b25cb
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-15 08:24:04 -0600

    fixed db name

--- dropaddtable.py
+++ dropaddtable.py
@@ -1,13 +1,20 @@
 import psycopg2
 
-conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 
 #Create a Table
 #The first step is to create a cursor. 
 
 cur = conn.cursor()
+cur.execute("DROP TABLE Tweetwordcount")
+conn.commit()
+
+
 cur.execute('''CREATE TABLE Tweetwordcount
        (word TEXT PRIMARY KEY     NOT NULL,
        count INT     NOT NULL);''')
 conn.commit()
+
+
+
 conn.close()
