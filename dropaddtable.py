72f067181075ae94e443d652bf43b51ddf62a4ef
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-15 08:22:54 -0600

    adding dropaddtable to start fresh

---
+++ dropaddtable.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+
+#Create a Table
+#The first step is to create a cursor. 
+
+cur = conn.cursor()
+cur.execute('''CREATE TABLE Tweetwordcount
+       (word TEXT PRIMARY KEY     NOT NULL,
+       count INT     NOT NULL);''')
+conn.commit()
+conn.close()
